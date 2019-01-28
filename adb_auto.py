a835a0500ca74af592c3c7f7d9092dcec60b02f4
王凡 <wangfan@botpy.com>
2019-01-28 12:19:20 +0800

    fix

--- adb_auto.py
+++ adb_auto.py
@@ -12,14 +12,30 @@
 import os
 import time
 import re
+import sys
+import datetime
+import logging
 
-ip = '10.10.9.189'
-password = '0805'
-os.system('adb connect {}'.format(ip))  # 连接手机
-size = os.popen('adb shell wm size')  # 获取手机屏幕分辨率
-x, y = re.findall(r"(\d+)x(\d+)", size.read())[0]
-x = int(x)
-y = int(y)
+logging.basicConfig(level=logging.INFO,filename='./log',
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+x = 0
+y = 0
+
+def connect():
+    global x
+    global y
+    ip = '10.10.9.189'
+    password = '0805'
+    res = os.system('adb connect {}'.format(ip))  # 连接手机
+    if res == 0:
+        size = os.popen('adb shell wm size')  # 获取手机屏幕分辨率
+        x, y = re.findall(r"(\d+)x(\d+)", size.read())[0]
+        x = int(x)
+        y = int(y)
+    else:
+        print('adb error')
+        sys.exit()
 
 def unlock(password):
     """
@@ -30,20 +46,7 @@ def unlock(password):
     os.system('adb shell input keyevent 224')  # 亮屏
     os.system('adb shell input swipe 300 1000 300 500')  # 滑动屏幕到输入密码页
     os.system('adb shell input text {}'.format(password))  # 输入密码,华为手机不需要点击确定
-    # os.system('adb shell input keyevent 164')  # 设置静音
-
-def run_wxxq():
-    """
-    网易星球自动收钻
-    :return:
-    """
-    #todo
-    os.system('adb shell monkey -p com.netease.blockchain -c android.intent.category.LAUNCHER 1')
-    for _ in range(2):  # 重复执行两次
-        time.sleep(5)
-        for x in range(0, 1080, 10):
-            for y in range(340, 1449, 10):  # 只点击黑钻可能出现的位置
-                os.system('adb shell input tap {} {}'.format(x, y))
+    
 
 def run_qywx(x, y):
     """
@@ -55,11 +58,25 @@ def run_qywx(x, y):
     # 运行企业微信
     os.system('adb shell monkey -p com.tencent.wework -c android.intent.category.LAUNCHER 1')
     time.sleep(5)  # 等待页面加载
-    os.system('adb shell input tap {} {}'.format(x-380, y-100))  # 屏幕坐标x,y值
-    os.system('adb shell input tap {} {}'.format(x-880, y-1560))
+    os.system('adb shell input tap 700 2060'.format(x-380, y-100))  # 屏幕坐标x,y值
+    os.system('adb shell input tap 200 600'.format(x-880, y-1560))
     time.sleep(5)
-    os.system('adb shell input tap {} {}'.format(x-480, y-560))
+    os.system('adb shell input tap 600 1600'.format(x-480, y-560))
+    os.system('adb shell input keyevent 223')
+
 
 if __name__ == '__main__':
-    unlock(password)
-    run_qywx()
+    while 1:
+        d=datetime.datetime.now()
+        w = d.isoweekday()
+        h = d.hour
+        m = d.minute
+        logger.info("当前时间{}".format(d))
+        if w not in (6,7) and h == 19 and m == 00:
+            logger.info("开始自动打卡")
+            connect()
+            unlock(password)
+            # run_wxxq()d
+            run_qywx(x, y)
+            logger.info("自动打卡完成")
+        time.sleep(60)
